dc1854e2ae2839e81034a54719e2649b659ad21d
moujikovitch <sbst.legall@gmail.com>
2015-04-29 17:05:42 +0200

    upload v3

---
+++ jetprivee.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'shotgun'
+require 'sinatra/activerecord'
+
+set :database,  "sqlite3:donnees_db.sqlite3"
+
+class Dataavion < ActiveRecord::Base
+end
+
+get '/' do
+  erb :index
+end
+
+get '/menu' do
+	@datas=Dataavion.all
+  	erb :menu
+end
+
+get '/add' do
+	erb :add
+end
+
+post '/add' do
+	Dataavion.create(params)
+	redirect '/'
+end
